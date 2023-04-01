2e19ce76a0a82c4d692d43908f3e15b24b8d833e
Ellen Regan <Ellen.Regan@Mondrian.com>
2023-04-01 12:10:46 +0100

    Switch to using composite dropdown parameter as input

--- Composite_Main/CUMULATIVE_COMPOSITE.py
+++ Composite_Main/CUMULATIVE_COMPOSITE.py
@@ -1,20 +1,16 @@
 #%%
 from common.common_module import *
 
-composite_code = "'INT_EQ'"
-valuation_date = "'2023-02-28'"
-reporting_currency = "'USD'"
-
 #%%
-def runDenodo_Cumulative_Composite_Performance():
+def runDenodo_Cumulative_Composite_Performance(composite_code,reporting_currency,valuation_date):
     
     #missing inception date and market value
 
-    Sql = "select * from mondrian.i_cor_composite_performance where period_length = 'MONTH' and expressed_currency_iso = " + reporting_currency + " and composite_code =" + composite_code + " and valuation_date =" + valuation_date
+    Sql = "select * from mondrian.i_cor_composite_performance where period_length = 'MONTH' and expressed_currency_iso = '" + reporting_currency + "' and composite_code ='" + composite_code + "' and valuation_date ='" + valuation_date + "'"
 
 
     data = run_sql("", "mondrian",command = Sql, database_type="Denodo")
     return data
 
-cumulative_comp_perf_output = runDenodo_Cumulative_Composite_Performance()
+#cumulative_comp_perf_output = runDenodo_Cumulative_Composite_Performance()
 
--- pages/composite.py
+++ pages/composite.py
@@ -4,66 +4,87 @@ from dash import Dash, html, dcc, callback, Output, Input, dash_table
 import dash
 from common.common_module import *
 import plotly.express as px
-from Composite_Main.CUMULATIVE_COMPOSITE import cumulative_comp_perf_output
+from Composite_Main.CUMULATIVE_COMPOSITE import runDenodo_Cumulative_Composite_Performance
 from Composite_Main.COMPOSITE_LIST import composite_list
 from Composite_Main.COMPOSITE_DISCLOSURE import composite_disclosure
 import plotly.graph_objects as go
 
 dash.register_page(__name__)
-df_ccp = cumulative_comp_perf_output
+
 df_comp = composite_list
+options_dict = dict(zip(df_comp['composite_name'], df_comp['composite_code']))
 dff_comp = composite_list.composite_name
 df_cdisc = composite_disclosure
 
-#%%
-#Composite Cumulative Performance
-dff_ccp =df_ccp.sort_values(by='period_ending')
-fig =go.Figure()
-#traces
-fig.add_trace(
-    go.Scatter(
-    x=dff_ccp.period_ending, y=dff_ccp.indexed_composite_performance, name = 'Composite Gross Performance', line = dict(color='firebrick', width = 4)))
-fig.add_trace(
-    go.Scatter(
-    x=dff_ccp.period_ending, y=dff_ccp.indexed_composite_net_performance, name = 'Composite Net Performance', line = dict(color='royalblue', width = 4)))
-fig.add_trace(
-    go.Scatter(
-    x=dff_ccp.period_ending, y=dff_ccp.indexed_benchmark_performance, name = 'Benchmark Performance', line = dict(color='darkgreen', width = 4)))
-
-
-fig.update_layout(title='Cumulative Composite Performance',
-                  xaxis_title = 'Year',
-                  yaxis_title='Return')
-fig.show()
-
+x = "INT_EQ"
+z = "2023-02-28"
+y = "USD"
 
 #%%
-#Composite & Fee Scale Description
-dff_cdisc = df_cdisc.composite_description + '\n' + df_cdisc.fee_scale_description
-
 #layout
 layout = html.Div([
     html.H1(children='Composite Dashboard', style={'textAlign':'center'}),
     dcc.Dropdown(
-        options=[{'label':i, 'value':i} for i in dff_comp.unique()],
-        value = 'International Equity', 
+        options=[{'label':composite_name, 'value':composite_code} for composite_name,composite_code in options_dict.items()],
+        value = 'INT_EQ', 
         id='dropdown-selection'),
     html.Div(id='dd-output-container'),
     dcc.Graph(
         id="cumulative_composite",
-        figure=fig,
+        figure={},
         style={'width':'50%', 'height': '30%'}),
     html.H3(children='Composite Disclosure',style={'textAlign':'left'}),
     html.Div(df_cdisc.composite_description, style = {'colour': 'black', 'fontSize': 36, 'width':'50%'}),
     html.H3(children='Fee Scale',style={'textAlign':'left'}),
     html.Div(df_cdisc.fee_scale_description, style = {'colour': 'black', 'fontSize': 36, 'width':'50%'})
 ])
-
+"""
 @callback(
     Output('dd-output-container', 'children'),
     Input('dropdown-selection', 'value')
 )
 def update_output(value):
-    return f'You have selected {value}'
+    composite_code_value = df_comp.query('composite_name'==value,)['composite_code']
+    return composite_code_value
+    
+comp_value = update_output('dd-output-container')
+"""
+
+#%%
+#Composite Cumulative Performance
+
+@callback(
+    Output(component_id='cumulative_composite', component_property='figure'),
+    Input(component_id = 'dropdown-selection', component_property='value')
+)
+def cumulative_fig_ccpure(col_chosen):
+    df_ccp = runDenodo_Cumulative_Composite_Performance(composite_code=col_chosen,reporting_currency=y,valuation_date=z)
+    dff_ccp =df_ccp.sort_values(by='period_ending')
+    fig_ccp =go.Figure()
+    #traces
+    fig_ccp.add_trace(
+        go.Scatter(
+        x=dff_ccp.period_ending, y=dff_ccp.indexed_composite_performance, name = 'Composite Gross Performance', line = dict(color='firebrick', width = 4)))
+    fig_ccp.add_trace(
+        go.Scatter(
+        x=dff_ccp.period_ending, y=dff_ccp.indexed_composite_net_performance, name = 'Composite Net Performance', line = dict(color='royalblue', width = 4)))
+    fig_ccp.add_trace(
+        go.Scatter(
+        x=dff_ccp.period_ending, y=dff_ccp.indexed_benchmark_performance, name = 'Benchmark Performance', line = dict(color='darkgreen', width = 4)))
+
+
+    fig_ccp.update_layout(title='Cumulative Composite Performance',
+                    xaxis_title = 'Year',
+                    yaxis_title='Return')
+    return fig_ccp
+
+
+    
+
+#%%
+#Composite & Fee Scale Description
+#dff_cdisc = df_cdisc.composite_description + '\n' + df_cdisc.fee_scale_description
+
+
 
 
